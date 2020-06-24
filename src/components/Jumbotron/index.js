77ab338c0fd12932fc72c482072965d3093649fb
FCTESLA <fcamacho888@gmail.com>
2020-06-23 20:11:47 -0700

    started building components

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css"
+
+
+function Jumbotron() {
+
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container" >
+                <h1 className="display-4"> Employee Directory</h1> 
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
